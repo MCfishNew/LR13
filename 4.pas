e6ed37061196092356233a3dfeeea34cbd4f9ad0
MCfishNew <103477341+MCfishNew@users.noreply.github.com>
2022-12-16 11:09:19 +0300

    Add files via upload

---
+++ 4.pas
@@ -0,0 +1,11 @@
+﻿var
+   filetext: text;
+   a:string;
+begin
+write('Введите строку: ');
+readln(a);
+assign(filetext,'C:\PABCWork.NET\text4.txt');
+append(filetext);
+write(filetext,a);
+close(filetext);
+end.
---
+++ 7.pas
@@ -0,0 +1,22 @@
+﻿var
+F_in, F_out: Text;
+i, j, k, s, n: integer;
+
+begin
+assign(F_in, 'C:\PABCWork.NET\text7_in.txt');
+reset(F_in);
+assign(F_out, 'C:\PABCWork.NET\text7_out.txt');
+rewrite(F_out);
+read(F_in, n);
+for i := 1 to n do
+begin
+k := 0;
+for j := 1 to i do
+if i mod j = 0 then inc(k);
+if k = 5 then
+S += i;
+end;
+writeln(F_out,S);
+close(F_in);
+close(F_out);
+end.
